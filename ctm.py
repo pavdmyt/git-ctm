c354033c20fc435723747731fc2f6502dc34ad4a
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-02-07 21:23:00 +0200

    add date template printing

--- ctm.py
+++ ctm.py
@@ -7,9 +7,11 @@ from docopt import docopt
 
 CLI_SPEC = """\
 Usage:  ctm -d <date> -m <msg>
+        ctm -p
 
   -d <date>      Commit date and time
   -m <msg>       Commit message
+  -p             Print date template
 
 """
 
@@ -19,10 +21,18 @@ CMD_TMPL = ("GIT_AUTHOR_DATE='{date}' "
             "git commit -m '{msg}'")
 
 
+DATE_TMPL = 'Sun Feb 7 11:11:11 2018 +0200'
+
+
 def main():
     args = docopt(CLI_SPEC)
     date = args['-d']
     msg = args['-m']
+    print_tmpl = args['-p']
+
+    if print_tmpl:
+        print(repr(DATE_TMPL))
+        return
 
     cmd = CMD_TMPL.format(**{'date': date, 'msg': msg})
     print(cmd)
